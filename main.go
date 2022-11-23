0d7a38e2dc638c6e8bc49789db1880f841e358b6
Minori Tokuda <snowcrush@mail.0pt.jp>
2022-11-23 15:59:36 +0900

    strip /rpc prefix in a traefik middleware

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 
 	"github.com/bufbuild/connect-go"
-	"github.com/go-chi/chi"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 	"google.golang.org/protobuf/types/known/emptypb"
@@ -51,12 +50,9 @@ var IsDevelopment = true
 
 func main() {
 	var server MessageStreamServer
-	router := chi.NewRouter()
 	_, handler := protoconnect.NewMessageStreamHandler(&server)
-	handler = http.StripPrefix("/rpc", handler)
-	router.Handle("/rpc/*", handler)
 	http.ListenAndServe(
 		"localhost:6500",
-		h2c.NewHandler(router, &http2.Server{}),
+		h2c.NewHandler(handler, &http2.Server{}),
 	)
 }
