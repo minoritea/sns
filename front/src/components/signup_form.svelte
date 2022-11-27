46350aa32b57baec0e4cb3c8ce0a806b6f9d1faa
Minori Tokuda <snowcrush@mail.0pt.jp>
2022-11-28 01:30:14 +0900

    use svelte as UI component

---
+++ front/src/components/signup_form.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { authentication } from "~/store/session"
+	import client from "~/lib/authentication_client"
+
+	let name = ""
+	let password = ""
+	
+	function signUp() {
+    event.preventDefault()
+		authentication(client.signUp(name, password))
+	}
+
+	$: submitDisabled = name === "" || password === ""
+</script>
+
+<form on:submit={signUp}>
+	<label htmlFor="name">Name</label>
+	<input name="name" type="text" bind:value={name} />
+	<label htmlFor="password">Password</label>
+	<input name="password" type="password" bind:value={password} />
+	<input type="submit" disabled={submitDisabled} value="Sign up" />
+</form>
---
+++ front/src/components/top.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { onMount } from "svelte"
+	import MessageList from "~/components/message_list.svelte"
+	import MessageForm from "~/components/message_form.svelte"
+	import SignUpForm from "~/components/signup_form.svelte"
+	import client from "~/lib/authentication_client"
+	import session, { authentication } from "~/store/session"
+
+	onMount(() => {
+		authentication(client.isSignedIn())
+	})
+
+	$: isSignedIn = $session === "authenticated"
+</script>
+
+{#if isSignedIn}
+	<MessageForm />
+{:else}
+	<SignUpForm />
+{/if}
+<MessageList />
