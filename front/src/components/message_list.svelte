b408ef62b1049ccfdd5d533a039f0308ae7397bd
Minori Tokuda <snowcrush@mail.0pt.jp>
2022-11-28 12:30:25 +0900

    add styles to message list

--- front/src/components/message_list.svelte
+++ front/src/components/message_list.svelte
@@ -16,6 +16,45 @@
 
 <ul>
 { #each messages as message }
-	<li>{ message.body }</li>
+	<li class="message">
+			<img src="/person.svg" />
+			<div class="box">
+				<div class="userName">
+					@{ message.userName }
+				</div>
+				<div class="body">
+					{ message.body }
+				</div>
+			</div>
+	</li>
 { /each }
 </ul>
+
+<style>
+	li.message {
+		margin: 1rem 0;
+		display: flex;
+		align-items: center;
+	}
+
+	li.message .box {
+		flex: auto;
+		display: flex;
+		flex-direction: column;
+	}
+
+	li.message > img {
+		object-fit: cover;
+		width: 5rem;
+		height: 5rem;
+	}
+
+	li.message div.userName {
+		margin: 0 0 0 1rem;
+		font-size: 125%;
+	}
+
+	li.message div.body {
+		margin: 1rem 0 0 1rem;
+	}
+</style>
