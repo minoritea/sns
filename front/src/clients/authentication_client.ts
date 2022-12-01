207725fe7b373ecb0cc3f5183ac314495f58db11
Minori Tokuda <snowcrush@mail.0pt.jp>
2022-12-01 22:05:21 +0900

    split authentication forms into signin and signup pages

--- front/src/clients/authentication_client.ts
+++ front/src/clients/authentication_client.ts
@@ -1,24 +1,4 @@
 import { createConnectTransport, createPromiseClient } from "@bufbuild/connect-web"
-import type { PromiseClient } from "@bufbuild/connect-web"
 import { AuthenticationService } from "~/proto/authentication_connectweb"
-import { SignUpRequest } from "~/proto/authentication_pb"
 
-export class AuthenticationClient {
-  client: PromiseClient<typeof AuthenticationService>;
-  constructor(serverUrl: string) {
-    const transport = createConnectTransport({
-      baseUrl: serverUrl
-    })
-    this.client = createPromiseClient(AuthenticationService, transport)
-  }
-
-  async signUp(name: string, email: string, password: string): Promise<void> {
-    await this.client.signUp(new SignUpRequest({email, name, password}))
-  }
-
-  async isSignedIn(): Promise<void> {
-    await this.client.isSignedIn(null)
-  }
-}
-
-export default new AuthenticationClient("/rpc")
+export default createPromiseClient(AuthenticationService, createConnectTransport({ baseUrl: "/rpc" }))
--- front/src/components/top.svelte
+++ front/src/components/top.svelte
@@ -2,22 +2,20 @@
 	import { onMount } from "svelte"
 	import MessageList from "~/components/message_list.svelte"
 	import MessageForm from "~/components/message_form.svelte"
-	import SignUpForm from "~/components/signup_form.svelte"
+	import AuthenticationForm from "~/components/authentication_form.svelte"
 	import client from "~/clients/authentication_client"
 	import session, { authentication } from "~/stores/session"
 
 	onMount(() => {
-		authentication(client.isSignedIn())
+		authentication(client.isSignedIn({}))
 	})
-
-	$: isSignedIn = $session === "authenticated"
 </script>
 
 {#if $session === "authenticated"}
 	<MessageForm />
 	<MessageList />
 {:else if $session === "unauthenticated"}
-	<SignUpForm />
+	<AuthenticationForm />
 {:else}
 	<div class="text-white">loading...</div>
 {/if}
