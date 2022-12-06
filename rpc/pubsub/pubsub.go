e5c3ba40f8f544c3b1bca1cdf020d1594c12ef07
Minori Tokuda <snowcrush@mail.0pt.jp>
2022-12-06 22:36:04 +0900

    Pubsub can publish to groups classified by ID (#9)
    * ignore all .db files
    
    * fix Makefile
    
    * pubsub can publish to groups classified by ID

--- rpc/pubsub/pubsub.go
+++ rpc/pubsub/pubsub.go
@@ -4,14 +4,14 @@ import (
 	"sync"
 )
 
-type PubSub[P any, S any] struct {
-	sync.Mutex
-	subscribers map[chan P]S
+type PubSub[P any, S comparable] struct {
+	sync.RWMutex
+	subscribers map[S]map[chan P]struct{}
 }
 
-func New[P any, S any]() *PubSub[P, S] {
+func New[P any, S comparable]() *PubSub[P, S] {
 	var pubsub PubSub[P, S]
-	pubsub.subscribers = make(map[chan P]S)
+	pubsub.subscribers = make(map[S]map[chan P]struct{})
 	return &pubsub
 }
 
@@ -19,23 +19,43 @@ func (p *PubSub[P, S]) Subscribe(subscriber S) (chan P, func()) {
 	ch := make(chan P)
 	p.Lock()
 	defer p.Unlock()
-	p.subscribers[ch] = subscriber
+	if p.subscribers[subscriber] == nil {
+		p.subscribers[subscriber] = make(map[chan P]struct{})
+	}
+	p.subscribers[subscriber][ch] = struct{}{}
 	return ch, func() {
 		p.Lock()
 		defer p.Unlock()
-		delete(p.subscribers, ch)
+		delete(p.subscribers[subscriber], ch)
 	}
 }
 
-func (p *PubSub[P, S]) Publish(payload P, filters ...(func(S) bool)) {
-	p.Lock()
-	defer p.Unlock()
-	for ch, subscriber := range p.subscribers {
-		for _, filter := range filters {
-			if !filter(subscriber) {
-				continue
+func (p *PubSub[P, S]) publishToAll(payload P) {
+	for _, channels := range p.subscribers {
+		for ch := range channels {
+			ch <- payload
+		}
+	}
+}
+
+func (p *PubSub[P, S]) publishToSubscribers(payload P, subscribers []S) {
+	for _, subscriber := range subscribers {
+		channels := p.subscribers[subscriber]
+		if channels != nil {
+			for ch := range channels {
+				ch <- payload
 			}
 		}
-		ch <- payload
+	}
+}
+
+func (p *PubSub[P, S]) Publish(payload P, subscribers ...S) {
+	p.RLock()
+	defer p.RUnlock()
+
+	if len(subscribers) == 0 {
+		p.publishToAll(payload)
+	} else {
+		p.publishToSubscribers(payload, subscribers)
 	}
 }
